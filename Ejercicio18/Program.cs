d13e849691698a423e20fdbcb9937301987e55ba
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:30:15 -0600

    Ejercicio18

--- Ejercicio18/Program.cs
+++ Ejercicio18/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    // Método que recibe un string y lo imprime en reversa
+    static void ImprimirReversa(string texto)
+    {
+        char[] caracteres = texto.ToCharArray();
+        Array.Reverse(caracteres);
+        Console.WriteLine(new string(caracteres));
+    }
+
+    static void Main(string[] args)
+    {
+        // Solicitar al usuario que ingrese un texto
+        Console.Write("Ingrese un texto: ");
+        string texto = Console.ReadLine();
+
+        // Llamar al método para imprimir el texto en reversa
+        ImprimirReversa(texto);
+    }
+}
